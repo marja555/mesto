5d8eacb1d660a00eda2dac624d5410b4a14271b2
Marina Poselentseva <marja555@yandex.ru>
2021-11-17 15:59:44 +0500

    Исправить html названия кнопок, добавить к ним атрибуты, обернуть текст в футере

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,23 @@
 let popup = document.querySelector('.popup');
 let pageContainer = document.querySelector('.page__container');
 let profile = pageContainer.querySelector('.profile');
-let editButton = profile.querySelector('.profile__editButton');
-
-function openPopup() {
-  popup.classList.add('popup_opened');
-}
-
-editButton.addEventListener('click', openPopup);
-
+let editButton = profile.querySelector('.profile__edit-button');
 let closeButton = popup.querySelector('.popup__close');
-
-function closePopup() {
-  popup.classList.remove('popup_opened');
-}
-
-closeButton.addEventListener('click', closePopup);
-
 let formElement = document.querySelector('.form');
 let nameInput = formElement.querySelector('.form__text_type_name');
 let jobInput = formElement.querySelector('.form__text_type_profession');
 let profileName = document.querySelector('.profile__name');
 let profileProfession = document.querySelector('.profile__profession');
-nameInput.value = profileName.textContent;
-jobInput.value = profileProfession.textContent;
 
+function openPopup() {
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileProfession.textContent;
+  popup.classList.add('popup_opened');
+}
+
+function closePopup() {
+  popup.classList.remove('popup_opened');
+}
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
@@ -33,9 +26,6 @@ function formSubmitHandler (evt) {
   closePopup();
 }
 
-function formSubmitDone () {
-  alert('Готово!');
-}
-
+editButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
 formElement.addEventListener('submit', formSubmitHandler);
-formElement.addEventListener('click', formSubmitDone);
