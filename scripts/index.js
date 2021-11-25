0e66d381d6052923eb4550aa24758065d0a409cd
Marina Poselentseva <marja555@yandex.ru>
2021-11-26 00:14:47 +0500

    Сделать активными кнопки Удалить и Лайк

--- scripts/index.js
+++ scripts/index.js
@@ -29,3 +29,56 @@ function formSubmitHandler (evt) {
 editButton.addEventListener('click', openPopup);
 closeButton.addEventListener('click', closePopup);
 formElement.addEventListener('submit', formSubmitHandler);
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+]; 
+
+for (let i = 0; i < initialCards.length; i++) {
+  const cardTemplate = document.querySelector('#cardTemplate').content;
+  const cardsOnline = document.querySelector('.cards');
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  cardElement.querySelector('.card__title').textContent = initialCards[i].name;
+  cardElement.querySelector('.card__image').src = initialCards[i].link;
+  cardsOnline.append(cardElement);
+}
+
+const cardDelButtons = document.querySelectorAll('.card__del-button');
+for (let i = 0; i < cardDelButtons.length; i++) {
+  cardDelButtons[i].addEventListener('click', function (event) {
+    const eventTarget = event.target;
+    const cardItem = eventTarget.closest('.card');
+    cardItem.remove();
+  });
+}
+
+const likeButtons = document.querySelectorAll('.card__like');
+for (let i = 0; i < likeButtons.length; i++) {
+  likeButtons[i].addEventListener('click', function (event) {
+    const eventTarget = event.target;
+    eventTarget.classList.toggle('card__like_active');
+  });
+}
